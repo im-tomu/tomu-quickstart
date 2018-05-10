2da1f68fb54c82547bf74f2c124d356a3406870b
Sean Cross <sean@xobs.io>
2018-05-10 12:33:09 -0700

    usb_msc: add mass storage example
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ usb_msc/README.md
@@ -0,0 +1,4 @@
+# README
+
+This example implements a USB Mass Storage Class (MSC) device
+to demonstrate the use of the USB device stack.
---
+++ usb_msc/usb_msc.c
@@ -0,0 +1,183 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2013 Weston Schmidt <weston_schmidt@alumni.purdue.edu>
+ * Copyright (C) 2013 Pavol Rusnak <stick@gk2.sk>
+ * Copyright (C) 2018 Seb Holzapfel <schnommus@gmail.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \addtogroup Examples
+ *
+ * This example implements a USB CDC-ACM device (aka Virtual Serial Port)
+ * to demonstrate the use of the USB device stack.
+ *
+ * When data is recieved, it will toggle the green LED and echo the data.
+ * The red LED is toggled constantly and a string is sent over USB every
+ * time the LED changes state as a heartbeat.
+ */
+
+#include <libopencm3/cm3/common.h>
+#include <libopencm3/cm3/vector.h>
+#include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/efm32/wdog.h>
+#include <libopencm3/efm32/gpio.h>
+#include <libopencm3/efm32/cmu.h>
+#include <libopencm3/usb/usbd.h>
+#include <libopencm3/usb/msc.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ramdisk.h"
+
+/* Default AHB (core clock) frequency of Tomu board */
+#define AHB_FREQUENCY 14000000
+
+#define LED_GREEN_PORT GPIOA
+#define LED_GREEN_PIN  GPIO0
+#define LED_RED_PORT   GPIOB
+#define LED_RED_PIN    GPIO7
+
+#define VENDOR_ID                 0x1209    /* pid.code */
+#define PRODUCT_ID                0x70b1    /* Assigned to Tomu project */
+#define DEVICE_VER                0x0101    /* Program version */
+
+usbd_device *g_usbd_dev = 0;
+
+static const struct usb_device_descriptor dev_descr = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0110,
+	.bDeviceClass = 0,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = VENDOR_ID,
+	.idProduct = PRODUCT_ID,
+	.bcdDevice = DEVICE_VER,
+	.iManufacturer = 1,
+	.iProduct = 2,
+	.iSerialNumber = 3,
+	.bNumConfigurations = 1,
+};
+
+static const struct usb_endpoint_descriptor msc_endp[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x01,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 0,
+}, {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x82,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 0,
+}};
+
+static const struct usb_interface_descriptor msc_iface[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_MSC,
+	.bInterfaceSubClass = USB_MSC_SUBCLASS_SCSI,
+	.bInterfaceProtocol = USB_MSC_PROTOCOL_BBB,
+	.iInterface = 0,
+	.endpoint = msc_endp,
+	.extra = NULL,
+	.extralen = 0
+}};
+
+static const struct usb_interface ifaces[] = {{
+	.num_altsetting = 1,
+	.altsetting = msc_iface,
+}};
+
+static const struct usb_config_descriptor config_descr = {
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+	.bNumInterfaces = 1,
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0x80,
+	.bMaxPower = 0x32,
+	.interface = ifaces,
+};
+
+static const char *usb_strings[] = {
+	"Tomu",
+	"MSC Demo",
+	"DEMO",
+};
+
+void usb_isr(void)
+{
+    usbd_poll(g_usbd_dev);
+    gpio_toggle(LED_GREEN_PORT, LED_GREEN_PIN);
+}
+
+void hard_fault_handler(void)
+{
+    while(1);
+}
+
+/* Buffer to be used for control requests. */
+static uint8_t usbd_control_buffer[128];
+
+int main(void)
+{
+    int i;
+
+    /* Make sure the vector table is relocated correctly (after the Tomu bootloader) */
+    SCB_VTOR = 0x4000;
+
+    /* Disable the watchdog that the bootloader started. */
+    WDOG_CTRL = 0;
+
+    /* GPIO peripheral clock is necessary for us to set up the GPIO pins as outputs */
+    cmu_periph_clock_enable(CMU_GPIO);
+
+    /* Set up both LEDs as outputs */
+    gpio_mode_setup(LED_RED_PORT, GPIO_MODE_WIRED_AND, LED_RED_PIN);
+    gpio_mode_setup(LED_GREEN_PORT, GPIO_MODE_WIRED_AND, LED_GREEN_PIN);
+
+    /* Configure the USB core & stack */
+	g_usbd_dev = usbd_init(&efm32hg_usb_driver, &dev_descr, &config_descr,
+			               usb_strings, 3,
+			               usbd_control_buffer, sizeof(usbd_control_buffer));
+
+
+	ramdisk_init();
+	usb_msc_init(g_usbd_dev, 0x82, 64, 0x01, 64, "Tomu", "RamdiskExample",
+                 "0.00", ramdisk_blocks(), ramdisk_read, ramdisk_write);
+
+    /* Enable USB IRQs */
+	nvic_enable_irq(NVIC_USB_IRQ);
+
+    while(1) {
+        gpio_toggle(LED_RED_PORT, LED_RED_PIN);
+        for(i = 0; i != 500000; ++i)
+			__asm__("nop");
+    }
+}
